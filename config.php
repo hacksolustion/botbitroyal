a7c9a1467997bd59977eabbe072a98194c5b8f38
Hack Solustion <50937775+hacksolustion@users.noreply.github.com>
2019-05-28 19:31:49 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+//Masukkan PHPSESSID
+$PHPSESSID = 'c7ct30hphm1b5t0euftpnk8ke6';
+//Masukkan GA
+$_ga = 'GA1.2.419714075.1552753165';
+//Masukkan GID
+$_gid = 'GA1.2.318855109.1552753165';
+
+
+?>
